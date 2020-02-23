5fd2c4716fc9be4d61f422531fe8c1d91264b1b1
sam <sam.cormack@gmail.com>
2020-02-23 21:25:02 +1300

    Data imports done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import MainPanel from './components/MainPanel'
 import Sidebar from './components/SideBar'
 import './App.css';
+import { getIncomeData } from './util/transform'
+
 
 function App() {
+  const [income, ] = useState(getIncomeData)
+
   return (
     <div className="App">
+      {console.log(income)}
       <MainPanel/>
       <Sidebar/>
     </div>
---
+++ src/util/transform.js
@@ -0,0 +1,67 @@
+import Papa from 'papaparse'
+import * as R from 'ramda'
+import incomeData from '../data/income_data_processed.csv'
+import popData from '../data/population_age.csv'
+
+const papaPromise = path => {
+  return new Promise((resolve, reject) => {
+    Papa.parse(path, {download: true, dynamicTyping: true, complete: ({data, errors}) => {
+      if (R.isEmpty(errors)) {
+        resolve(data)
+      } else {
+        reject(errors)
+      }
+    }})
+  })
+}
+
+function transformData(rawData) {
+  return R.zipObj(
+    R.head(rawData), R.transpose(R.tail(rawData))
+  )
+}
+
+function popOverAge(minAge, population) {
+  return R.reduce(
+    (acc, [pop,]) => acc + pop,
+    0,
+    R.filter(
+      ([, age]) => age >= minAge, 
+      R.zip(population.population, population.age_lower_bound)
+    )
+  )
+}
+
+function nPeopleCorrected(nPeopleRaw, population) {
+  return R.adjust(
+    0,
+    noIncRaw => noIncRaw + popOverAge(15, population) - R.sum(nPeopleRaw),
+    nPeopleRaw
+  )
+}
+
+// inc.df$cumulative_people = cumsum(inc.df$n_people)
+// inc.df$cumulative_proportion = inc.df$cumulative_people/over15pop
+
+const cumeSum = R.compose(R.tail, R.scan(R.add, 0))
+
+function cumeProp(nPeople) {
+  const nTot = R.sum(nPeople)
+  return R.map(R.divide(R.__, nTot), cumeSum(nPeople))
+}
+
+async function getIncomeData() {
+  const rawData = await Promise.all([
+    papaPromise(incomeData),
+    papaPromise(popData)
+  ])
+  const [income, population] = R.map(transformData, rawData)
+  const nPeople = nPeopleCorrected(income.n_people, population)
+  return {
+    taxableIncome: income.taxable_income_per_person,
+    nPeople: nPeople,
+    cumeProp: cumeProp(nPeople),
+  }
+}
+
+export { getIncomeData }
