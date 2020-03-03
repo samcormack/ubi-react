6211e89cd8eabdc951c747859dc2ac3192f89045
sam <sam.cormack@gmail.com>
2020-03-03 21:05:37 +1300

    Messing with graphs

--- src/components/UbiGraphs.js
+++ src/components/UbiGraphs.js
@@ -7,6 +7,9 @@ const graphify = R.zipWith((x,y) =>{return {x, y}} )
 function UbiGraphs({cumeProp, beforeTaxIncome, initialAfterTaxIncome, afterTaxIncome}) {
   return (
     <VictoryChart
+      height = {600}
+      width = {800}
+      padding = {{left: 60, top:40, right:20, bottom:40}}
       // animate = {{ duration: 100, easing: "sinInOut" }}  
     >
       <VictoryGroup
